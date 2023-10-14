f9b06d3bab59b2bbe5cc2c5c6f5467a9546e2e74
hafialiassajjad <aliassajjad04@gmail.com>
2023-10-14 22:46:06 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----------------------------------------------------------"<< endl  ;
+    cout << "Hello world!"<< endl << endl ;
+    cout << "Nama: hafiz ali assajjad!"<< endl << endl;
+    cout << "NPM: 1910631170190"<< endl << endl;
+    cout << "Kelas 1A Informatika!" << endl << endl;
+    cout << "Cita-cita : programmer" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Karena Tertarik di Bidang IT" << endl << endl;
+    cout << "Asal : Karawang" << endl << endl;
+    cout << "----------------------------------------------------------"<< endl ;
+
+    return 0;
+}
